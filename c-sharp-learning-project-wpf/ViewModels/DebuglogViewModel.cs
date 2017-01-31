c05f535bdd91fc366f17ce27cf1427fc3e01256d
TORI <chronoborder@gmail.com>
2017-01-31 15:05:47 +0900

    [add] ログ用MVVMを追加
    + まだ機能としては動かない

---
+++ c-sharp-learning-project-wpf/ViewModels/DebuglogViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_learning_project_wpf.ViewModels
+{
+    class DebuglogViewModel : INotifyPropertyChanged, IDataErrorInfo
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string this[string columnName]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Error
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
